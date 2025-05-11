bd084fd6f7a7a6b3b5e41ef91df73225a54a694c
Ser Martin <ser.martinovskiy@yandex.ru>
2025-05-11 07:40:29 +0300

    First Commit

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -4,6 +4,8 @@ class MainPageLocators:
     TITLE_MAIN_PAGE = (By.XPATH, '//h1[contains(text(),"Соберите бургер")]')
     ORDERED_FEED_BUTTON = (By.XPATH, '//p[contains(text(),"Лента Заказов")]')
     PERSONAL_ACCOUNT_BUTTON = (By.XPATH, '//p[contains(text(),"Личный Кабинет")]')
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[contains(text(),'Конструктор')]")
+
 
 
 
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,2 +1,18 @@
 import allure
 from data import *
+from pages.main_page import MainPage
+from locators.main_page_locators import MainPageLocators
+
+
+
+
+class TestMainPageFunction:
+    @allure.title('')
+    def test_transfer_click_to_button_constructor(self, driver):
+        main_page = MainPage(driver)
+        main_page.open_start_window()
+        main_page.click_personal_account_button()
+        main_page.click_element(MainPageLocators.CONSTRUCTOR_BUTTON)
+        assert main_page.wait_and_find_element(MainPageLocators.TITLE_MAIN_PAGE).text == 'Соберите бургер'
+
+
